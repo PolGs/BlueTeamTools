b9b40d2eae8878d9e6b5e841acb05565176b47c5
PolGs <polgalvezsoriano@gmail.com>
2024-04-17 23:28:30 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify, send_file, url_for
 import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
+from flask_cors import CORS
+import time
+import random
+import string 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/extract-html', methods=['GET'])
 def extract_html():
@@ -21,20 +26,38 @@ def extract_html():
 @app.route('/screenshot', methods=['GET'])
 def screenshot():
     url = request.args.get('url')
+    url2 = url
     if not url:
         return jsonify({"error": "URL parameter is required"}), 400
 
-    screenshot_path = os.path.join('static', 'images', f"{url.split('//')[1].split('/')[0]}_screenshot.png")
-    options = Options()
-    options.headless = True
+    #screenshot_path = os.path.join('static', 'images', f"{url.split('//')[1].split('/')[0]}_screenshot.png")
+    screenshot_path = "screen" + generate_random_string(10) + ".png"
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Ensure GUI is off
+    chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+
     try:
-        driver = webdriver.Chrome(options=options)
-        driver.get(url)
-        driver.save_screenshot(screenshot_path)
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.get(url2)
+        driver.save_screenshot("static/"+screenshot_path)
         driver.quit()
-        return send_file(screenshot_path, mimetype='image/png')
+        return get_image_link(screenshot_path)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+def get_image_link(filename):
+    # Ensure that the file exists in the static directory (you can add additional validation here)
+    # Generate the URL for the image file
+    image_url = url_for('static', filename=filename, _external=True)
+    return jsonify({'image_url': image_url})
+
+def generate_random_string(length):
+    # Define the characters to use in the random string
+    characters = string.ascii_letters + string.digits
+    # Generate a random string
+    random_string = ''.join(random.choice(characters) for i in range(length))
+    return random_string
 
 if __name__ == '__main__':
     app.run(debug=True)
